9bc45ec49df53f7898aa888fc2cf556e92d168eb
Virginia Mesa <virginia.mesa@auctane.com>
2023-06-28 19:13:15 +0200

    Release production

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getEnvVariables } from "../helpers/getEnvVariables";
 
-const { VITE_API_URL , HEROKU_API_URL } = getEnvVariables()
+const { VITE_API_URL } = getEnvVariables()
 
 const api = axios.create({ 
     baseURL: VITE_API_URL
